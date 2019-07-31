968daf252508183db2b2a846bc5d6debdfa04c54
wreulicke <saito.masaya@classmethod.jp>
2019-07-31 10:41:34 +0900

    add docs

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
 
 	output: {
 		filename: '[name].[chunkhash].js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'docs')
 	},
 
 	plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin({
